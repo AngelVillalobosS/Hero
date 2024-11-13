3e7e9b247af5f619350467978aeaf5750c84d717
AngelVillalobosS <angel.villalobos2269@uppuebla.edu.mx>
2024-11-12 21:49:21 -0600

    ProjectBase

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+<header id="header">
+    <div class="inner">
+        <div class="cotainer">
+            <!-- Logo -->
+            <h1><a href="{{ route('home') }}" id="logo">HERO</a></h1>
+
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li class="current_page_item"><a href="{{ route('home') }}">Inicio</a></li>
+                    <li>
+                        <a href="#">Trabajos</a>
+                        <ul>
+                            <li><a href="#">Pintura</a></li>
+                            <li><a href="#">Ojalatería</a></li>
+                            <li>
+                                <a href="#">Reparaciones</a>
+                                <ul>
+                                    <li><a href="#">Choques</a></li>
+                                    <li><a href="#">Abolladuras</a></li>
+                                    <li><a href="#">Enmacillado</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="left-sidebar.html">Agendar cita</a></li>
+                    <li><a href="right-sidebar.html">Galería de trabajos</a></li>
+                    <li><a href="no-sidebar.html">Marcas</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
